fae3f0c0c7207dca377bb7610c70540cc6f892c6
AlSidorenko <alek.sidorenko1979@gmail.com>
2019-10-10 18:41:49 +0300

    BLOCK_02. Bubble Sort.

---
+++ src/main/java/ua/tef/BLOCK02/trainingcod/BubbleSort_del/BubbleSort.java
@@ -0,0 +1,23 @@
+package ua.tef.BLOCK02.trainingcod.BubbleSort_del;
+
+/**
+ * Created on 10.10.2019 17:35.
+ *
+ * @author Aleks Sidorenko (e-mail: alek.sidorenko@gmail.com).
+ * @version Id$.
+ * @since 0.1.
+ */
+public class BubbleSort {
+
+    public void sort(int[] array) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/ua/tef/BLOCK02/trainingcod/BubbleSort_del/BubbleSortStart.java
@@ -0,0 +1,25 @@
+package ua.tef.BLOCK02.trainingcod.BubbleSort_del;
+
+import java.util.Arrays;
+
+/**
+ * Created on 10.10.2019 17:36.
+ *
+ * @author Aleks Sidorenko (e-mail: alek.sidorenko@gmail.com).
+ * @version Id$.
+ * @since 0.1.
+ */
+public class BubbleSortStart {
+
+    public static void main(String[] args) {
+
+        long timeStart = System.currentTimeMillis();
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] array = {-25, 87, 124, 5, 33, 53, 3};
+        System.out.printf("Array is not sorting: %s\n", Arrays.toString(array));
+        bubbleSort.sort(array);
+        System.out.printf("Array is sorting: %s\n", Arrays.toString(array));
+        long timeFinish = System.currentTimeMillis();
+        System.out.printf("Time: %s ms.", timeFinish - timeStart);
+    }
+}
