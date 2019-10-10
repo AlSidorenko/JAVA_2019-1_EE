b6b86f3eb26b3b4c67bda4ad5be72e3f1f6ff94e
AlSidorenko <alek.sidorenko1979@gmail.com>
2019-10-10 16:24:22 +0300

    BLOCK_02. Selection Sorter.

---
+++ src/main/java/ua/tef/BLOCK02/trainingcod/SelectionSorter/SelectionSorter.java
@@ -0,0 +1,23 @@
+package ua.tef.BLOCK02.trainingcod.SelectionSorter;
+
+/**
+ * Created on 10.10.2019 16:01.
+ *
+ * @author Aleks Sidorenko (e-mail: alek.sidorenko@gmail.com).
+ * @version Id$.
+ * @since 0.1.
+ */
+public class SelectionSorter {
+
+    public static void sort(int[] arr) {
+        for (int barrier = 0; barrier < arr.length - 1; barrier++) {
+            for (int index = barrier + 1; index < arr.length; index++) {
+                if (arr[barrier] > arr[index]) {
+                    int tmp = arr[index];
+                    arr[index] = arr[barrier];
+                    arr[barrier] = tmp;
+                }
+            }
+        }
+    }
+}
