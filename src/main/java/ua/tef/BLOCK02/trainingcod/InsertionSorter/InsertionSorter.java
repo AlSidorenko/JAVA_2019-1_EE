dc24aa3fa13a60b1f526c30df1f5ddb6cd387d91
AlSidorenko <alek.sidorenko1979@gmail.com>
2019-10-10 16:22:52 +0300

    BLOCK_02. Insertion Sorter.

---
+++ src/main/java/ua/tef/BLOCK02/trainingcod/InsertionSorter/InsertionSorter.java
@@ -0,0 +1,23 @@
+package ua.tef.BLOCK02.trainingcod.InsertionSorter;
+
+/**
+ * Created on 10.10.2019 16:10.
+ *
+ * @author Aleks Sidorenko (e-mail: alek.sidorenko@gmail.com).
+ * @version Id$.
+ * @since 0.1.
+ */
+public class InsertionSorter {
+
+    public static void sort(int[] arr) {
+        for (int k = 1; k < arr.length; k++) {
+            int newElement = arr[k];
+            int location = k - 1;
+            while (location >= 0 && arr[location] > newElement) {
+                arr[location + 1] = arr[location];
+                location--;
+            }
+            arr[location + 1] = newElement;
+        }
+    }
+}
